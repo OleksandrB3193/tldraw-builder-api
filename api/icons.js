82c76d5f9e9bfbb813171d02dee32e5e977ed6bd
OleksandrB3193 <oleksandr.b@tvory.tech>
2025-04-30 09:50:15 +0300

    Refactor icon API handler to support pagination and set limit to 30 icons per request

--- api/icons.js
+++ api/icons.js
@@ -22,13 +22,17 @@ const oauth = new OAuth(
 const handler = async (req, res) => {
   cors()(req, res, async () => {
     try {
-      const { query } = req.query;
+      const { query, next_page } = req.query;
 
       if (!query) {
         return res.status(400).json({ error: "Query is required" });
       }
 
-      const searchUrl = `https://api.thenounproject.com/v2/icon?query=${query}&limit=40&limit_to_public_domain=1`;
+      const limit = 30; // Set the limit to 5 icons per request
+
+      const searchUrl = next_page
+        ? `https://api.thenounproject.com/v2/icon?query=${query}&next_page=${next_page}&limit=${limit}&limit_to_public_domain=1`
+        : `https://api.thenounproject.com/v2/icon?query=${query}&limit=${limit}&limit_to_public_domain=1`;
 
       oauth.get(searchUrl, "", "", (err, result) => {
         if (err) {
@@ -36,8 +40,15 @@ const handler = async (req, res) => {
           return res.status(500).json({ error: "Failed to fetch data" });
         }
 
-        const icons = JSON.parse(result).icons;
-        res.json(icons);
+        const parsedResult = JSON.parse(result);
+        if (!parsedResult.icons || parsedResult.icons.length === 0) {
+          return res.status(404).json({ error: "No icons found" });
+        }
+
+        const next_page_token = parsedResult.next_page || null;
+        const icons = parsedResult.icons;
+
+        res.json({ icons, next_page: next_page_token });
       });
     } catch (error) {
       console.error("Server error:", error);
